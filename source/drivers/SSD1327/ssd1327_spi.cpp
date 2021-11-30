2ab5fb0df541ce7274e73bbfeb26b0cd3d0ff7bc
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-11-30 10:57:15 +0000

    Add ssd1327 dirver

---
+++ source/drivers/SSD1327/ssd1327_spi.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+
+#include "ssd1327.h"
+
+using namespace codal;
+using namespace std;
+
+SSD1327_SPI::SSD1327_SPI(STM32SPI* spi, STM32Pin* chipSelect, STM32Pin* dataCommand, unsigned width, unsigned height)
+    : SSD1327(width, height), spi(spi), cs(chipSelect), dc(dataCommand)
+{
+    cs->setDigitalValue(1);
+    dc->setDigitalValue(0);
+}
+
+SSD1327_SPI::~SSD1327_SPI() {}
+
+void SSD1327_SPI::writeCommand(uint8_t cmd)
+{
+    dc->setDigitalValue(0);
+    cs->setDigitalValue(0);
+
+    spi->write(cmd);
+
+    cs->setDigitalValue(1);
+}
+
+void SSD1327_SPI::writeData(uint8_t* buf, unsigned len)
+{
+    cs->setDigitalValue(0);
+
+    dc->setDigitalValue(0);
+    spi->beginTransaction();
+
+    spi->writeTransaction(0x15);  // Set Column Address
+    spi->writeTransaction(0x00);  // Start Column Address
+    spi->writeTransaction(0x3F);  // End Column Address
+    spi->writeTransaction(0x75);  // Set Row Address (segment)
+    spi->writeTransaction(0x00);  // Satrt Row Address
+    spi->writeTransaction(0x7F);  // End Row Address
+
+    spi->endTransaction();
+
+    dc->setDigitalValue(1);
+    spi->beginTransaction();
+
+    for (unsigned i = 0; i < len; ++i) {
+        spi->writeTransaction(buf[i]);
+    }
+
+    spi->endTransaction();
+
+    cs->setDigitalValue(1);
+}
--- source/frameBuffer.cpp
+++ source/frameBuffer.cpp
@@ -1,75 +1,106 @@
 #include "frameBuffer.h"
+
 #include "font.h"
 
 using namespace codal;
 using namespace std;
 
-FrameBuffer::FrameBuffer( unsigned widthPixel, unsigned heightPixel, FrameBuffer::Format format ) 
-    : width(widthPixel), height(heightPixel), format(format){
-
-    switch( this->format ){
+FrameBuffer::FrameBuffer(unsigned widthPixel, unsigned heightPixel, FrameBuffer::Format format)
+    : width(widthPixel), height(heightPixel), format(format)
+{
+    switch (this->format) {
         case Format::MONO_VLSB:
-            pages = height / 8;
+            pages      = height / 8;
             bufferSize = pages * width;
+            break;
+
+        case Format::GS4_HMSB:
+            pages      = 0;
+            bufferSize = height * (width / 2);
             break;
     }
 
-
     buffer = new uint8_t[bufferSize];
-    
 }
 
-FrameBuffer::~FrameBuffer(){
+FrameBuffer::~FrameBuffer()
+{
     delete[] buffer;
 }
 
-void FrameBuffer::fill( uint16_t color ){
-
-    switch( format ){
+void FrameBuffer::fill(uint16_t color)
+{
+    switch (format) {
         case Format::MONO_VLSB:
-            for( unsigned i = 0; i < bufferSize; ++i ){
+            for (unsigned i = 0; i < bufferSize; ++i) {
+                buffer[i] = color > 0 ? 0xFF : 0x00;
+            }
+            break;
+
+        case Format::GS4_HMSB:
+            for (unsigned i = 0; i < bufferSize; ++i) {
                 buffer[i] = color > 0 ? 0xFF : 0x00;
             }
             break;
     }
 }
 
-void FrameBuffer::drawPixel( unsigned x, unsigned y, uint16_t color){
+void FrameBuffer::drawPixel(unsigned x, unsigned y, uint16_t color)
+{
+    if (x >= width || y >= height) {
+        return;
+    }
 
-    if( x >= width || y >= height ){ return; }
+    switch (format) {
+        case Format::MONO_VLSB: {
+            int shift     = y % 8;
+            int index     = x + ((y / 8) * width);
+            uint8_t* data = &buffer[index];
 
-    switch( format ){
-        case Format::MONO_VLSB:
-            int shift = y % 8;
-            int index = x + ( ( y / 8 ) * width );
-            uint8_t * data = &buffer[index];
-
-            if(color > 0){
+            if (color > 0) {
                 *data |= (0x01 << shift);
             }
-            else{
+            else {
                 *data &= ~(0x01 << shift);
             }
             break;
+        }
+
+        case Format::GS4_HMSB: {
+            int shift     = (x & 0x01) == 1 ? 0x00 : 0x04;
+            uint8_t value = color & 0x000F;
+            uint8_t* data = &buffer[y * height + (x / 2)];
+
+            if (x & 0x01 == 1) {
+                *data &= 0xF0;
+                *data |= value;
+            }
+            else {
+                *data &= 0x0F;
+                *data |= (value << 4);
+            }
+
+            break;
+        }
     }
 }
 
-void FrameBuffer::drawChar( char c, unsigned x, unsigned y, uint16_t color){
-    
-    for( int i = 0; i < 5; ++i){
+void FrameBuffer::drawChar(char c, unsigned x, unsigned y, uint16_t color)
+{
+    for (int i = 0; i < 5; ++i) {
+        uint8_t currentChar = ASCII_FONT[c * 5 + i];
 
-        uint8_t currentChar = ASCII_FONT[ c * 5 + i ];
-
-        for(int j = 0; j < 8; ++j){
-            if( ( currentChar & ( 0x01 << j) ) > 0 ){
-                drawPixel( x + i, y + j, color);
+        for (int j = 0; j < 8; ++j) {
+            if ((currentChar & (0x01 << j)) > 0) {
+                drawPixel(x + i, y + j, color);
             }
         }
     }
 }
 
-void FrameBuffer::drawText(string str, unsigned x, unsigned y, uint16_t color){
-    for( size_t i = 0; i < str.length(); ++i ){
-        drawChar( str[i], x + i * 6, y, color );
+void FrameBuffer::drawText(string str, unsigned x, unsigned y, uint16_t color)
+{
+    for (size_t i = 0; i < str.length(); ++i) {
+        drawChar(str[i], x + i * 6, y, color);
     }
 }
